0f371bbf5c0f3c6e05e4dfb5493b4b768672b0be
Annika Santhanam <AnnikaSanthanam@Annikas-MacBook.lan>
2023-10-31 23:03:46 -0400

    First commit message

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+//glitch stuff 
+
+//
+
+app.use(express.static('public'))
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+      });
+    
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+  });
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
